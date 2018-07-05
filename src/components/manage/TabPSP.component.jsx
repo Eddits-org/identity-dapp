ebea91263e8893fda9a9a25c1a516562cd79c736
thomas.brillard <thomas.brillard@intech.lu>
2018-07-05 09:43:05 +0200

    Some css

--- src/components/manage/TabPSP.component.jsx
+++ src/components/manage/TabPSP.component.jsx
@@ -15,7 +15,7 @@ class TabPSPComponent extends React.Component {
 
     return (
       <div className='content'>
-        <table className='table is-fullwidth'>
+        <table className='table is-fullwidth monospaced'>
           <thead>
           <tr>
             <th>Date</th>
