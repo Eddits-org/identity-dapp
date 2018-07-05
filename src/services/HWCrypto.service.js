ddbfb0578b0080b21226c54958ce2b33a7eda67c
Fabrice CROISEAUX <fcroiseaux@gmail.com>
2018-07-05 22:03:50 +0200

    Read Cert ok

--- src/services/HWCrypto.service.js
+++ src/services/HWCrypto.service.js
@@ -1,16 +1,21 @@
 const config = require('config');
 const HWCrypto = require('assets/lib/hwcrypto.js');
+const ASN1 = require('ASN1js');
+const PKI = require('PKIjs');
 
 const { dappURL } = config;
 
 class HWCryptoService {
   getCertificate(address) {
       return HWCrypto.getCertificate({lang: 'en'}).then((response) => {
-          const cert = response;
-          console.log('Using certificate: $(cert.hex)');
+          console.log('RESPONSE HEX : ' + response.hex);
+          const asn1 = ASN1.fromBER(response.hex);
+          console.log('ASN1 : ' + asn1);
+          const cert = new Certificate({ schema: asn1.result });
+          console.log('Using certificate: ' + cert);
           return cert;
       }, function(err) {
-          console.log('getCertificate() failed: $(err)');
+          console.log('getCertificate() failed: ' + err.message);
       });
   }
 }
